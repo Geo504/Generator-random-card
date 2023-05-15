3be9c8127e3e4a7123f1805aa9ffe30800ee2901
Geovanny Valladares <129303891+Geo504@users.noreply.github.com>
2023-05-15 11:31:31 +0000

    First version

--- src/app.js
+++ src/app.js
@@ -5,7 +5,44 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const card = {
+  heart: ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
+  spades: ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
+  clover: ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
+  diamond: ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
 };
+
+const getRandomItem = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
+};
+const getRandomNumber = (array) => {
+  return [Math.floor(Math.random() * array.length)];
+};
+
+const getRandomCard = () => {
+  let randomKey = getRandomItem(Object.keys(card));
+  let randomCard = card[randomKey][getRandomNumber(card[randomKey])];
+
+  const divCard = document.querySelector("#randomCard");
+
+  divCard.style.display = "block";
+
+  randomKey === "heart" || randomKey === "diamond"
+    ? (divCard.style.color = "darkred")
+    : (divCard.style.color = "black");
+
+  let icon = "fa-heart";
+  if (randomKey === "diamond") icon = "fa-diamond";
+  if (randomKey === "clover") icon = "fa-clover";
+  if (randomKey === "spades") icon = "fa-spa";
+
+  divCard.innerHTML = `
+    <span><i class="fa-solid ${icon}"></i></span>
+    <p class="cardNumber">${randomCard}</p>
+    <span class="d-flex"><i class="fa-solid ${icon} ms-auto"></i></span>
+    `;
+};
+
+document
+  .getElementById("generateCard")
+  .addEventListener("click", getRandomCard, false);
