e75ffaa0f4d6a3b6ab4201894c8309169b3b459b
Geovanny Valladares <129303891+Geo504@users.noreply.github.com>
2023-05-16 11:34:18 +0000

    v4.0

--- src/app.js
+++ src/app.js
@@ -5,48 +5,56 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-const card = {
+const cards = {
   "♥": ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
   "♠": ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
   "♣": ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
-  "♦": ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
+  "♦": ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
 };
 
-const getRandomItem = (array) => {
+const getRandomItem = array => {
   return array[Math.floor(Math.random() * array.length)];
 };
-const getRandomNumber = (array) => {
+const getRandomNumber = array => {
   return [Math.floor(Math.random() * array.length)];
 };
 
 const getRandomCard = () => {
-  let randomKey = getRandomItem(Object.keys(card));
-  let indexCard = getRandomNumber(card[randomKey]);
-  let randomCard = card[randomKey][indexCard];
-
   const divCard = document.querySelector("#randomCard");
-
   divCard.style.background = "white";
 
-  randomKey === "♥" || randomKey === "♦"
-    ? (divCard.style.color = "darkred")
-    : (divCard.style.color = "black");
-
-  let icon = "♥";
-  if (randomKey === "♦") icon = "♦";
-  if (randomKey === "♣") icon = "♣";
-  if (randomKey === "♠") icon = "♠";
-
-  divCard.innerHTML = `
-    <span class="col-12"><i>${icon}</i></span>
-    <p class="cardNumber">${randomCard}</p>
-    <span class="col-12 d-flex"><i class="ms-auto">${icon}</i></span>
+  if (Object.keys(cards).length === 0) {
+    divCard.innerHTML = `
+    <span class="col-12">Out of Cards</span>
+    <p class="cardNumber">🤡</p>
+    <span class="col-12 text-end">Out of Cards</span>
     `;
+  }
 
-  console.log(`${randomKey} : ${randomCard} // ${card[randomKey]}`);
-  card[randomKey].splice(indexCard, 1);
+  if (Object.keys(cards).length > 0) {
+    let randomKey = getRandomItem(Object.keys(cards));
+    let indexCard = getRandomNumber(cards[randomKey]);
+    let randomCard = cards[randomKey][indexCard];
 
-  if (card[randomKey].length === 0) delete card[randomKey];
+    randomKey === "♥" || randomKey === "♦"
+      ? (divCard.style.color = "darkred")
+      : (divCard.style.color = "black");
+
+    let icon = "♥";
+    if (randomKey === "♦") icon = "♦";
+    if (randomKey === "♣") icon = "♣";
+    if (randomKey === "♠") icon = "♠";
+
+    divCard.innerHTML = `
+      <span class="col-12"><i>${icon}</i></span>
+      <p class="cardNumber">${randomCard}</p>
+      <span class="col-12 d-flex"><i class="ms-auto">${icon}</i></span>
+      `;
+
+    cards[randomKey].splice(indexCard, 1);
+
+    if (cards[randomKey].length === 0) delete cards[randomKey];
+  }
 };
 
 const getDeckOfCards = () => {
