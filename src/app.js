23d48da940b2a983c0b2373f3301093184c06727
Geovanny Valladares <129303891+Geo504@users.noreply.github.com>
2023-05-23 12:19:55 +0000

    correction made

--- src/app.js
+++ src/app.js
@@ -7,71 +7,77 @@ import "./assets/img/4geeks.ico";
 
 const divCard = document.querySelector("#randomCard");
 
-const cards = {
+const getDeck =()=>{
+  const DECKCARDS = {
+    "♥": ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
+    "♠": ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
+    "♣": ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
+    "♦": ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+  };
+  return DECKCARDS;
+}
+
+let cards = {
   "♥": ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
   "♠": ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
   "♣": ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
   "♦": ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
 };
 
+const span = document.createElement('span');
+const paragraph = document.createElement('p');
+const invertedSpan = document.createElement('span');
+
+span.className = 'col-12';
+paragraph.className = 'cardNumber';
+invertedSpan.className = 'col-12';
+invertedSpan.style.transform = 'rotate(180deg)';
+divCard.append(span, paragraph, invertedSpan);
+
 const getRandomItem = array => {
   return array[Math.floor(Math.random() * array.length)];
 };
+
 const getRandomNumber = array => {
   return [Math.floor(Math.random() * array.length)];
 };
 
-const getRandomCard = () => {
-  const span = document.createElement("span");
-  const paragraph = document.createElement("p");
-  const invertedSpan = document.createElement("span");
-
-  span.className = "col-12";
-  paragraph.className = "cardNumber";
-  invertedSpan.className = "col-12";
-  invertedSpan.style.transform = "rotate(180deg)";
-
-  while (divCard.hasChildNodes()) {
-    divCard.removeChild(divCard.firstChild);
+const renderRandomCard = () => {
+  if (!Object.keys(cards).length) {
+    span.textContent = 'Out of Cards';
+    paragraph.textContent = '🤡';
+    invertedSpan.textContent = 'Out of Cards';
+    return;
   }
+  
+  let randomKey = getRandomItem(Object.keys(cards));
+  let indexCard = getRandomNumber(cards[randomKey]);
+  let randomCard = cards[randomKey][indexCard];
 
-  if (Object.keys(cards).length === 0) {
-    span.textContent = "Out of Cards";
-    paragraph.textContent = "🤡";
-    invertedSpan.textContent = "Out of Cards";
-  }
+  randomKey === "♥" || randomKey === "♦"
+    ? (divCard.className = "card col-sm-5 col-10 red")
+    : (divCard.className = "card col-sm-5 col-10 black");
 
-  if (Object.keys(cards).length > 0) {
-    let randomKey = getRandomItem(Object.keys(cards));
-    let indexCard = getRandomNumber(cards[randomKey]);
-    let randomCard = cards[randomKey][indexCard];
+  span.textContent = `${randomKey}`;
+  paragraph.textContent = `${randomCard}`;
+  invertedSpan.textContent = `${randomKey}`;
 
-    randomKey === "♥" || randomKey === "♦"
-      ? (divCard.className = "card col-sm-5 col-10 red")
-      : (divCard.className = "card col-sm-5 col-10 black");
-
-    let icon = "♥";
-    if (randomKey === "♦") icon = "♦";
-    if (randomKey === "♣") icon = "♣";
-    if (randomKey === "♠") icon = "♠";
-
-    span.textContent = `${icon}`;
-    paragraph.textContent = `${randomCard}`;
-    invertedSpan.textContent = `${icon}`;
-
-    cards[randomKey].splice(indexCard, 1);
-    if (cards[randomKey].length === 0) delete cards[randomKey];
-  }
-  divCard.append(span, paragraph, invertedSpan);
+  cards[randomKey].splice(indexCard, 1);
+  if (cards[randomKey].length === 0) delete cards[randomKey];
 };
 
 const getDeckOfCards = () => {
-  return location.reload();
+  cards = getDeck();
+  divCard.className = "card col-sm-5 col-10"
+  span.textContent = '';
+  paragraph.textContent = '';
+  invertedSpan.textContent = '';
+  return cards;
 };
 
 document
   .getElementById("generateCard")
-  .addEventListener("click", getRandomCard, false);
+  .addEventListener("click", renderRandomCard, false);
 
 document
   .getElementById("generateDeck")
